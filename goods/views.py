aaf058dac6eeccb9ac9b33a7b4312aa6b3b25ee9
Zontax <jrvadim04@gmail.com>
2024-03-18 23:53:11 +0200

    pagination in parameters

--- goods/views.py
+++ goods/views.py
@@ -3,14 +3,16 @@ from django.http import Http404, HttpResponseNotFound
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from goods.models import Product
 
-def catalog(request, category_slug, page=1):
+def catalog(request, category_slug):
+    page = request.GET.get('page', 1)
+    
     if category_slug == 'all':
         goods = Product.objects.all()
     else:
         goods = get_list_or_404(Product.objects.filter(category__slug=category_slug))
         
     paginator = Paginator(goods, 3)
-    current_page = paginator.page(page);    
+    current_page = paginator.page(int(page));    
     
     context = {
         'title': 'Каталог',
@@ -23,7 +25,7 @@ def catalog(request, category_slug, page=1):
 def product(request, product_slug):
     product = Product.objects.get(slug=product_slug)
     context = {
-        'title': 'Товар',
+        'title': product.name,
         'product': product, 
     }
     return render(request, 'goods/product.html', context)
