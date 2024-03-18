ef08108c9824060f88abaff340c452dd1f0ab9c9
Zontax <jrvadim04@gmail.com>
2024-03-18 21:52:12 +0200

    Slug conventer for categories

--- goods/views.py
+++ goods/views.py
@@ -1,17 +1,27 @@
-from django.shortcuts import render
-
+from django.http import Http404, HttpResponseNotFound
+from django.shortcuts import render, get_object_or_404
 from goods.models import Product
 
-def catalog(request):
-    goods = Product.objects.all()
+def catalog(request, category_slug):
+    if category_slug == 'all':
+        goods = Product.objects.all()
+    else:
+        goods = Product.objects.filter(category__slug=category_slug)
+        if not goods.exists():
+            raise Http404('Товарів для цієї категорії неіснує')
+        
     context = {
         'title': 'Каталог',
         'goods': goods,
+        'category_slug': category_slug
     }
     return render(request, 'goods/catalog.html', context)
 
-def product(request):
+
+def product(request, product_slug):
+    product = Product.objects.get(slug=product_slug)
     context = {
-        'title': 'Продукти',
+        'title': 'Товар',
+        'product': product, 
     }
     return render(request, 'goods/product.html', context)
--- main/context_processors.py
+++ main/context_processors.py
@@ -4,7 +4,7 @@ from goods.models import Category
 
 
 def base_processors(request):
-    categories = Category.objects.all()
+    categories = Category.objects.all().order_by('slug')
     current_year = datetime.now().year
     return { 'current_year': current_year, 'all_categories': categories  }
 
