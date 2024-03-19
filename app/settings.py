06d2f1fb42924e2b3509bed2981517473dd46c43
Zontax <jrvadim04@gmail.com>
2024-03-19 14:43:58 +0200

    Base user login, register, profile template

--- app/settings.py
+++ app/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     
     "main",
     "goods",
-    # "users",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -47,7 +47,7 @@ ROOT_URLCONF = "app.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
