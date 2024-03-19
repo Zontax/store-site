6aede98096efcb072297565e6c2902fb807a7db4
Zontax <jrvadim04@gmail.com>
2024-03-19 12:49:01 +0200

    Connect POSTGRESQL and add fulltext search

--- goods/utilitis.py
+++ goods/utilitis.py
@@ -1,5 +1,6 @@
 from goods.models import Product
 from django.db.models import Q
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 
 def q_search(query):
     
@@ -7,16 +8,20 @@ def q_search(query):
     if isinstance(query, str):
         if query.isdigit() and len(query) <= 5: 
             return Product.objects.filter(id=int(query)) 
-    
-        # Пошук по словах
-        keywords = [word for word in query.split() if len(word) > 2]
-        q_objects = Q()
         
-        for token in keywords:
-            q_objects |= Q(name__icontains=token)
-            q_objects |= Q(description__icontains=token)
+        # Пошук по словах (від postgre)
+        vector = SearchVector('name', 'description')
+        query = SearchQuery(query)
+        return Product.objects.annotate(rank=SearchRank(vector, query)).order_by('-rankє')
         
-        return Product.objects.filter(q_objects)
+        # keywords = [word for word in query.split() if len(word) > 2]
+        # q_objects = Q()
+        
+        # for token in keywords:
+        #     q_objects |= Q(name__icontains=token)
+        #     q_objects |= Q(description__icontains=token)
+        
+        # return Product.objects.filter(q_objects)
     
     else:
         return Product.objects.all()
