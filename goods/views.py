e19a7fc22d1d69cf7efcf6b1151bf3556e28b610
Zontax <jrvadim04@gmail.com>
2024-03-19 10:43:24 +0200

    fix pagination and add simple_tag change params for url params

--- goods/views.py
+++ goods/views.py
@@ -2,21 +2,30 @@ from django.core.paginator import Paginator
 from django.http import Http404, HttpResponseNotFound
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from goods.models import Product
+from app.settings import GOODS_IN_PAGE
 
-def catalog(request, category_slug):
+def catalog(request, category_slug=None):
     page = request.GET.get('page', 1)
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
     
-    if category_slug == 'all':
+    if category_slug == None or category_slug == 'all':
         goods = Product.objects.all()
     else:
         goods = get_list_or_404(Product.objects.filter(category__slug=category_slug))
         
-    paginator = Paginator(goods, 3)
-    current_page = paginator.page(int(page));    
+    if on_sale:
+        goods = goods.filter(discount__gt=0) # фільтр по знижці більшій за нуль
+        
+    if order_by and order_by != 'default':
+        goods = goods.order_by(order_by) # сортування
+    
+    paginator = Paginator(goods, GOODS_IN_PAGE)
+    goods_in_page = paginator.page(int(page));    
     
     context = {
         'title': 'Каталог',
-        'goods': current_page,
+        'goods': goods_in_page,
         'slug_url': category_slug
     }
     return render(request, 'goods/catalog.html', context)
