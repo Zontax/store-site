d761464aa86ed4ff8aad14efc636ae21f29675bc
Zontax <jrvadim04@gmail.com>
2024-03-19 13:54:51 +0200

    Full text search well done

--- app/settings.py
+++ app/settings.py
@@ -120,4 +120,4 @@ INTERNAL_IPS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-GOODS_IN_PAGE = 9
+GOODS_IN_PAGE = 6
--- goods/utilitis.py
+++ goods/utilitis.py
@@ -1,6 +1,6 @@
 from goods.models import Product
 from django.db.models import Q
-from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector, SearchHeadline
 
 def q_search(query):
     
@@ -12,16 +12,30 @@ def q_search(query):
         # Пошук по словах (від postgre)
         vector = SearchVector('name', 'description')
         query = SearchQuery(query)
-        return Product.objects.annotate(rank=SearchRank(vector, query)).order_by('-rankє')
+         
+        result = (
+            Product.objects
+                    .annotate(rank=SearchRank(vector, query))
+                    .filter(rank__gt=0)
+                    .order_by('-rank')
+        )
+        result = result.annotate(
+            headline=SearchHeadline(
+                'name',
+                query,
+                start_sel='<span style="background-color: yellow;">',
+                stop_sel='</span>',
+        ))
         
-        # keywords = [word for word in query.split() if len(word) > 2]
-        # q_objects = Q()
+        result = result.annotate(
+            bodyline=SearchHeadline(
+                'description',
+                query,
+                start_sel='<span style="background-color: yellow;">',
+                stop_sel='</span>',
+        ))
         
-        # for token in keywords:
-        #     q_objects |= Q(name__icontains=token)
-        #     q_objects |= Q(description__icontains=token)
+        return result
         
-        # return Product.objects.filter(q_objects)
-    
     else:
         return Product.objects.all()
